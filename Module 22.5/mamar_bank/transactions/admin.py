2b9228b3263ae1d777e40df78d0f0cdb5c7972be
saiful-islam-auny <aunychowdhury99@gmail.com>
2024-07-01 13:46:20 +0600

    practice 22.5 and 23.5

---
+++ Module 22.5/mamar_bank/transactions/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+# from transactions.models import Transaction
+from .models import Transaction
+from .models import Bankrupt
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['account', 'amount', 'balance_after_transaction', 'transaction_type', 'loan_approve']
+
+    
+    def save_model(self, request, obj, form, change):
+        obj.account.balance += obj.amount
+        obj.balance_after_transaction = obj.account.balance
+        obj.account.save()
+        super().save_model(request, obj, form, change)
+        
+
+@admin.register(Bankrupt)        
+class BankruptAdmin(admin.ModelAdmin):
+    list_display = ['is_bankrupt']
+
